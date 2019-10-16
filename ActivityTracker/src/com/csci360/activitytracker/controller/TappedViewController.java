3e5cec43627d05f3028ab80b0dd97262522c8ad5
Rex Ferrer <ferrergordonrb@g.cofc.edu>
2019-10-16 12:56:35 -0400

    Corrected package name to com.csci360.activitytracker. Refactored class and view names to proper convention. Refactored file structure. Added model, css, resources, controller packages

---
+++ ActivityTracker/src/com/csci360/activitytracker/controller/TappedViewController.java
@@ -0,0 +1,60 @@
+package com.csci360.activitytracker.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+//TODO Generate time and push to view. Generate data and push to view.
+
+public class TappedViewController implements Initializable  {
+	
+	@FXML
+	private AnchorPane complete;
+	@FXML 
+	private Label timeLabel;
+	@FXML
+	private ImageView settings;
+    private Parent root;
+    private Stage stage;
+    
+	@FXML
+	private void handleAction() throws IOException {
+	    System.out.println("You clicked me!");
+	    //switch scene
+	    stage = (Stage) settings.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("StepsView.fxml"));
+    
+	    Scene scene = new Scene(root);
+	    stage.setScene(scene);
+	    stage.show();
+	}
+
+	@FXML
+	private void handleSettings() throws IOException {
+		System.out.println("You clicked me!");
+	    //switch scene
+	    stage = (Stage) timeLabel.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("DataInputView.fxml"));
+    
+	    Scene scene = new Scene(root);
+	    stage.setScene(scene);
+	    stage.show();
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
