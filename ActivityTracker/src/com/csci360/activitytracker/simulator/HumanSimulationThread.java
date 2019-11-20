b636f8e9898ad6683f23657fc9701707fd067cb2
Rex B Ferrer <ferrergordonrb@g.cofc.edu>
2019-11-20 12:47:01 -0500

    - Modified Thread library used for HumanSimulationThread

--- ActivityTracker/src/com/csci360/activitytracker/simulator/HumanSimulationThread.java
+++ ActivityTracker/src/com/csci360/activitytracker/simulator/HumanSimulationThread.java
@@ -4,23 +4,17 @@ import com.csci360.activitytracker.simulator.model.BodyState;
 import com.csci360.activitytracker.simulator.model.Person;
 
 //Singleton Thread
-public class HumanSimulationThread extends Thread {
+public class HumanSimulationThread {
 
   private static HumanSimulationThread humanSimulationThread;
   private BodyState currentBodyState;
   private Person person;
 
+
   private HumanSimulationThread() {
   }
 
-  public synchronized HumanSimulationThread getInstance() {
-    if (humanSimulationThread == null) {
-      humanSimulationThread = new HumanSimulationThread();
-    }
-    return humanSimulationThread;
-  }
-
-  public void run() {
+  private void runSimulation() {
     person = person.getInstance();
 
     for (BodyState bodyState : person.getSchedule().getActivities()) {
@@ -32,6 +26,26 @@ public class HumanSimulationThread extends Thread {
         e.printStackTrace();
       }
     }
+  }
+
+  public void startSimulation() {
+    Runnable task = new Runnable() {
+      @Override
+      public void run() {
+        runSimulation();
+      }
+    };
+
+    Thread backgroundThread = new Thread(task);
+    backgroundThread.setDaemon(true);
+    backgroundThread.start();
+  }
+
+  public synchronized HumanSimulationThread getInstance() {
+    if (humanSimulationThread == null) {
+      humanSimulationThread = new HumanSimulationThread();
+    }
+    return humanSimulationThread;
   }
 
   private void setCurrentBodyState() {
