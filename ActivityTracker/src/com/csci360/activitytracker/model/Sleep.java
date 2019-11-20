1207b98ece85fffb2b3e011908967dc21e836d42
Rex B Ferrer <ferrergordonrb@g.cofc.edu>
2019-11-20 12:16:12 -0500

    - Added deep, light, total fields to Sleep object and necessary functions.

--- ActivityTracker/src/com/csci360/activitytracker/model/Sleep.java
+++ ActivityTracker/src/com/csci360/activitytracker/model/Sleep.java
@@ -1,7 +1,33 @@
 package com.csci360.activitytracker.model;
 
 public class Sleep {
-  // TODO: Determine appropriate data structure for these
-  Object time;
-  Object patterns;
+
+  private int deep = 0;
+  private int light = 0;
+  private int total = 0;
+
+  Sleep() {
+  }
+
+  public void incrementDeep(int increment) {
+    this.deep += increment;
+    this.total += increment;
+  }
+
+  public void incrementLight(int increment) {
+    this.light += increment;
+    this.total += increment;
+  }
+
+  public int getDeep() {
+    return this.deep;
+  }
+
+  public int getLight() {
+    return this.light;
+  }
+
+  public int getTotal() {
+    return this.total;
+  }
 }
